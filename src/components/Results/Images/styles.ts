c0deaf944ce18181a6de89f90bbaf047e9787de4
Алексеев Александр Сергеевич AlekSAlekseev <aleksalekseev@stud.kpfu.ru>
2024-07-25 19:39:37 +0400

    initial

---
+++ src/components/Results/Images/styles.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  max-width: 850px;
+  flex-flow: wrap;
+  margin: 10px auto;
+  gap: 10px;
+`;
